077597ad662def00589b2c17d36f5ab4e009a44a
Shambu Kumar <105148455+ShambuKumar@users.noreply.github.com>
2023-03-22 05:48:30 +0000

    Adeed namespace and new empty class fro position

--- move.h
+++ move.h
@@ -1,25 +1,29 @@
-class Move
+
+namespace CHAI
 {
-    int From;
-    int To;
-    // Flags
-    bool *IsCapture;
-    bool *IsPromotion;
-    bool *IsEnPassant;
-    bool *IsCastle;
-    bool *IsQCastle;
-    // Promotion
-    int PromotionPiece;
-    Move(int From, int To, bool IsCapture = false, bool IsPromotion = false, bool IsEnPassant = false, bool IsCastle = false, bool IsQCastle = false, int PromotionPiece = 0)
+    class Move
     {
-        //Set values from parameters
-        int From = this->From;
-        int To = this->To;
-        bool IsCapture = this->IsCapture;
-        bool IsPromotion = this->IsPromotion;
-        bool IsEnPassant = this->IsEnPassant;
-        bool IsCastle = this->IsCastle;
-        bool IsQCastle = this->IsQCastle;
-        int PromotionPiece = this->PromotionPiece;
+        int From;
+        int To;
+        // Flags
+        bool *IsCapture;
+        bool *IsPromotion;
+        bool *IsEnPassant;
+        bool *IsCastle;
+        bool *IsQCastle;
+        // Promotion
+        int PromotionPiece;
+        Move(int From, int To, bool IsCapture = false, bool IsPromotion = false, bool IsEnPassant = false, bool IsCastle = false, bool IsQCastle = false, int PromotionPiece = 0)
+        {
+            //Set values from parameters
+            int From = this->From;
+            int To = this->To;
+            bool IsCapture = this->IsCapture;
+            bool IsPromotion = this->IsPromotion;
+            bool IsEnPassant = this->IsEnPassant;
+            bool IsCastle = this->IsCastle;
+            bool IsQCastle = this->IsQCastle;
+            int PromotionPiece = this->PromotionPiece;
+        };
     };
 };
