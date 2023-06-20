153353619f4733c1280c69e2f26ab62f3e4a4d5b
Shambu Kumar <105148455+ShambuKumar@users.noreply.github.com>
2023-06-20 08:37:30 +0000

    Bug fix#3: Curly braces fix

--- src/movegen.h
+++ src/movegen.h
@@ -100,178 +100,180 @@ namespace CHAI
                             i++;
                         }
                     }
-                    else if (piece == PAWN)
-                    {
-                        // Direction the pawn is moving
-                        int dir = (side == WHITE) ? -1 : 1;
-                        int doubleFile = (side == WHITE) ? 6 : 1;
-                        // Check validity of attack to the right
-                        if (mailbox[mailbox64[startSquare] + 9 * dir] != -1)
-                        {
-                            if (GameState->color[mailbox[mailbox64[startSquare] + 9 * dir]] == enemySide)
-                            {
-                                if (attackedSquaresGen)
-                                {
-                                    attackedSquares.set(targetSquare);
-                                }
-                                else if (!pinnedPieces.test(startSquare))
-                                {
-                                    std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 9 * dir]) << std::endl;
-                                    GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 9 * dir]});
-                                }
-                            }
-                            else if (GameState->color[mailbox[mailbox64[startSquare] + 9 * dir]] == side && attackedSquaresGen)
-                            {
-                                defendedPieces.set(startSquare + 9 * dir);
-                            }
-                        }
-                        // Check validity of attack to the left
-                        if (mailbox[mailbox64[startSquare] + 11 * dir] != -1)
-                        {
-                            if (GameState->color[mailbox[mailbox64[startSquare] + 11 * dir]] == enemySide)
-                            {
-                                if (attackedSquaresGen)
-                                {
-                                    attackedSquares.set(targetSquare);
-                                }
-                                else if (!pinnedPieces.test(startSquare))
-                                {
-                                    std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 11 * dir]) << std::endl;
-                                    GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 11 * dir]});
-                                }
-                            }
-                            else if (GameState->color[mailbox[mailbox64[startSquare] + 11 * dir]] == side && attackedSquaresGen)
-                            {
-                                defendedPieces.set(startSquare + 11 * dir);
-                            }
-                        }
-                        // Move forward
-                        if (mailbox[mailbox64[startSquare] + 10 * dir] != -1 && GameState->color[mailbox[mailbox64[startSquare] + 10 * dir]] == EMPTY && !pinnedPieces.test(startSquare))
-                        {
-                            std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 10 * dir]) << std::endl;
-                            GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 10 * dir]});
-                        }
-                        // Double move
-                        if (row(startSquare) == doubleFile && GameState->color[mailbox[mailbox64[startSquare] + 20 * dir]] == EMPTY && mailbox[mailbox64[startSquare] + 20 * dir] != -1 && !pinnedPieces.test(startSquare))
-                        {
-                            std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 20 * dir]) << std::endl;
-                            GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 20 * dir]});
-                        }
-                    };
                 }
-                else if (slide[piece - 1])
+                else if (piece == PAWN)
                 {
-                    // It's a sliding piece
-                    // Loop thorugh all offsets
-                    for (int i = 0; i < 8;)
+                    // Direction the pawn is moving
+                    int dir = (side == WHITE) ? -1 : 1;
+                    int doubleFile = (side == WHITE) ? 6 : 1;
+                    // Check validity of attack to the right
+                    if (mailbox[mailbox64[startSquare] + 9 * dir] != -1)
                     {
-                        // Valid Offset
-                        if (offset[piece - 1][i] != 0)
+                        if (GameState->color[mailbox[mailbox64[startSquare] + 9 * dir]] == enemySide)
                         {
-                            // Reset range
-                            range = 1;
-                            // Calculate the target square(for moving) by multplying the offset by the range and passing it into the mailbox array
-                            targetSquare = mailbox[mailbox64[startSquare] + offset[piece - 1][i] * range];
-                            // Helper variable exclusively for the attackedSquaresGen. Helps to the count of the piece the attack is "going through"
-                            int ghostPiece = -1;
-                            // Until a valid targetSquare
-                            while (targetSquare != -1)
+                            if (attackedSquaresGen)
                             {
+                                attackedSquares.set(targetSquare);
+                            }
+                            else if (!pinnedPieces.test(startSquare))
+                            {
+                                std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 9 * dir]) << std::endl;
+                                GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 9 * dir]});
+                            }
+                        }
+                        else if (GameState->color[mailbox[mailbox64[startSquare] + 9 * dir]] == side && attackedSquaresGen)
+                        {
+                            defendedPieces.set(startSquare + 9 * dir);
+                        }
+                    }
+                    // Check validity of attack to the left
+                    if (mailbox[mailbox64[startSquare] + 11 * dir] != -1)
+                    {
+                        if (GameState->color[mailbox[mailbox64[startSquare] + 11 * dir]] == enemySide)
+                        {
+                            if (attackedSquaresGen)
+                            {
+                                attackedSquares.set(targetSquare);
+                            }
+                            else if (!pinnedPieces.test(startSquare))
+                            {
+                                std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 11 * dir]) << std::endl;
+                                GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 11 * dir]});
+                            }
+                        }
+                        else if (GameState->color[mailbox[mailbox64[startSquare] + 11 * dir]] == side && attackedSquaresGen)
+                        {
+                            defendedPieces.set(startSquare + 11 * dir);
+                        }
+                    }
+                    // Move forward
+                    if (mailbox[mailbox64[startSquare] + 10 * dir] != -1 && GameState->color[mailbox[mailbox64[startSquare] + 10 * dir]] == EMPTY && !pinnedPieces.test(startSquare))
+                    {
+                        std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 10 * dir]) << std::endl;
+                        GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 10 * dir]});
+                    }
+                    // Double move
+                    if (row(startSquare) == doubleFile && GameState->color[mailbox[mailbox64[startSquare] + 20 * dir]] == EMPTY && mailbox[mailbox64[startSquare] + 20 * dir] != -1 && !pinnedPieces.test(startSquare))
+                    {
+                        std::cout << Algebraic::convertToAlgebraic(startSquare, mailbox[mailbox64[startSquare] + 20 * dir]) << std::endl;
+                        GameState->moves.push_back({startSquare, mailbox[mailbox64[startSquare] + 20 * dir]});
+                    }
+                };
+            }
+            else if (slide[piece - 1])
+            {
+                // It's a sliding piece
+                // Loop thorugh all offsets
+                for (int i = 0; i < 8;)
+                {
+                    // Valid Offset
+                    if (offset[piece - 1][i] != 0)
+                    {
+                        // Reset range
+                        range = 1;
+                        // Calculate the target square(for moving) by multplying the offset by the range and passing it into the mailbox array
+                        targetSquare = mailbox[mailbox64[startSquare] + offset[piece - 1][i] * range];
+                        // Helper variable exclusively for the attackedSquaresGen. Helps to the count of the piece the attack is "going through"
+                        int ghostPiece = -1;
+                        // Until a valid targetSquare
+                        while (targetSquare != -1)
+                        {
 
-                                // Not moving on a square which one's one piece occupies
-                                if (GameState->color[targetSquare] != side)
+                            // Not moving on a square which one's one piece occupies
+                            if (GameState->color[targetSquare] != side)
+                            {
+                                // Capture
+                                if (enemySide == GameState->color[targetSquare])
                                 {
-                                    // Capture
-                                    if (enemySide == GameState->color[targetSquare])
+                                    if (attackedSquaresGen)
                                     {
-                                        if (attackedSquaresGen)
+                                        if (ghostPiece < 0)
                                         {
-                                            if (ghostPiece < 0)
+                                            ghostPiece = targetSquare;
+                                        }
+                                        else
+                                        {
+                                            if (!(GameState->color[targetSquare] == side) && GameState->piece[targetSquare] == KING)
                                             {
-                                                ghostPiece = targetSquare;
+                                                pinnedPieces.set(ghostPiece);
                                             }
                                             else
                                             {
-                                                if (!(GameState->color[targetSquare] == side) && GameState->piece[targetSquare] == KING)
-                                                {
-                                                    pinnedPieces.set(ghostPiece);
-                                                }
-                                                else
-                                                {
-                                                    break;
-                                                }
+                                                break;
                                             }
                                         }
-                                        else if (!pinnedPieces.test(startSquare))
-                                        {
-                                            std::cout << Algebraic::convertToAlgebraic(startSquare, targetSquare) << std::endl;
-                                            GameState->moves.push_back({startSquare, targetSquare});
-                                            break;
-                                        }
                                     }
-                                    // Move
-                                    else
+                                    else if (!pinnedPieces.test(startSquare))
+                                    {
+                                        std::cout << Algebraic::convertToAlgebraic(startSquare, targetSquare) << std::endl;
+                                        GameState->moves.push_back({startSquare, targetSquare});
+                                        break;
+                                    }
+                                }
+                                // Move
+                                else
+                                {
+
+                                    if (attackedSquaresGen)
                                     {
 
-                                        if (attackedSquaresGen)
+                                        if (ghostPiece < 0)
                                         {
-                                            if (ghostPiece < 0)
-                                            {
-                                                attackedSquares.set(targetSquare);
-                                            }
-                                        }
-                                        else if (!pinnedPieces.test(startSquare))
-                                        {
-                                            std::cout << Algebraic::convertToAlgebraic(startSquare, targetSquare) << std::endl;
-                                            GameState->moves.push_back({startSquare, targetSquare});
+                                            attackedSquares.set(targetSquare);
                                         }
                                     }
+                                    else if (!pinnedPieces.test(startSquare))
+                                    {
+                                        std::cout << Algebraic::convertToAlgebraic(startSquare, targetSquare) << std::endl;
+                                        GameState->moves.push_back({startSquare, targetSquare});
+                                    }
                                 }
-                                else if (GameState->color[targetSquare] == side && attackedSquaresGen)
-                                {
-                                    defendedPieces.set(targetSquare);
-                                    break;
-                                }
-                                // Ray length increases
-                                range++;
-                                // Recalculate targetSquare
-                                targetSquare = mailbox[mailbox64[startSquare] + offset[piece - 1][i] * range];
                             }
-                            i++;
+                            else if (GameState->color[targetSquare] == side && attackedSquaresGen)
+                            {
+                                defendedPieces.set(targetSquare);
+                                break;
+                            }
+                            // Ray length increases
+                            range++;
+                            // Recalculate targetSquare
+                            targetSquare = mailbox[mailbox64[startSquare] + offset[piece - 1][i] * range];
                         }
-                        else
-                        {
-                            break;
-                        }
-                    };
-                };
-            };
-            void generate(int side)
-            {
-                std::cout << "MOVES FOUND:\n";
-                // Loop through all squares
-                for (int square = 0; square < 64;)
-                {
-                    // Check if its not the opponents piece
-                    if (GameState->color[square] == side)
+                        i++;
+                    }
+                    else
                     {
-                        getMoves(GameState->piece[square], square, side, false);
-                    };
-                    square++;
+                        break;
+                    }
                 };
-            };
-            void updateAttackedSquares(int side)
+            }
+        }
+        void generate(int side)
+        {
+            std::cout << "MOVES FOUND:\n";
+            // Loop through all squares
+            for (int square = 0; square < 64;)
             {
-                // Loop through all squares
-                for (int square = 0; square < 64;)
+                // Check if its not the opponents piece
+                if (GameState->color[square] == side)
                 {
-                    // Check if its the opponents piece
-                    if (GameState->color[square] == side)
-                    {
-                        getMoves(GameState->piece[square], square, side, true);
-                    };
-                    square++;
+                    getMoves(GameState->piece[square], square, side, false);
                 };
+                square++;
             };
         };
-    };
+        void updateAttackedSquares(int side)
+        {
+            // Loop through all squares
+            for (int square = 0; square < 64;)
+            {
+                // Check if its the opponents piece
+                if (GameState->color[square] == side)
+                {
+                    getMoves(GameState->piece[square], square, side, true);
+                };
+                square++;
+            };
+        };
+    }
+}
