7c33488b4109118721be01c53753c36b1f8681dd
Shambu Kumar <105148455+ShambuKumar@users.noreply.github.com>
2023-06-22 17:17:28 +0000

    Add bitboards to cout wtih names and formatting

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -4,12 +4,13 @@ int main()
 {
     Aurora::Position GameState(WHITE);
     Aurora::MoveGen::initializePosition(GameState);
-    Aurora::FEN::parse("8/8/8/8/8/2p5/1n6/K7", GameState.piece, GameState.color);
+    Aurora::FEN::parse("8/8/8/8/8/5K2/6N1/7b", GameState.piece, GameState.color);
     Aurora::FEN::displayBoard(GameState.piece, GameState.color);
     std::cout << std::endl;
     using namespace Aurora::MoveGen;
     generate(WHITE);
     updateAttackedSquares(BLACK);
+    std::cout << "ATTACKED SQUARES:" << std::endl;
     for (int i = 0; i < 64; i++)
     {
         char output = (attackedSquares.test(i)) ? '0' : '+';
@@ -20,7 +21,19 @@ int main()
         std::cout << output << " ";
     };
     std::cout << std::endl;
+    std::cout << "PINNED PIECES:" << std::endl;
 
+    for (int i = 0; i < 64; i++)
+    {
+        char output = (pinnedPieces.test(i)) ? '0' : '+';
+        if (i % 8 == 0)
+        {
+            std::cout << std::endl;
+        }
+        std::cout << output << " ";
+    };
+    std::cout << std::endl;
+    std::cout << "DEFENDED PIECES:" << std::endl;
     for (int i = 0; i < 64; i++)
     {
         char output = (defendedPieces.test(i)) ? '0' : '+';
