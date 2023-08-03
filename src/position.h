4b2b3eb469dfa35140be1d67fec8605755c051c3
ShambuKumar <kumarshambu2010@gmail.com>
2023-08-03 10:04:43 +0200

    Added copyPosition option to Position constructor

--- src/position.h
+++ src/position.h
@@ -1,21 +1,33 @@
-#include <vector>
+#include "defs.h"
 #include "move.h"
-namespace Aurora
-{
-    class Position
-    {
-    public:
-        std::vector<Move> moves;
-        int color[64] = {};
-        int piece[64] = {};
-        int enemySide;
-        bool bQCastlingRights;
-        bool bKCastlingRights;
-        bool wQCastlingRights;
-        bool wKCastlingRights;
-        Position(int side)
-        {
-            enemySide = (side == WHITE) ? BLACK : WHITE;
+#include <cstddef>
+#include <vector>
+namespace Aurora {
+class Position {
+  public:
+    std::vector<Move> moves;
+    int color[64] = {};
+    int piece[64] = {};
+    int enemySide;
+    bool bQCastlingRights;
+    bool bKCastlingRights;
+    bool wQCastlingRights;
+    bool wKCastlingRights;
+    Position(int side, bool copyPosition = false,
+             Position *Position = nullptr) {
+        enemySide = (side == WHITE) ? BLACK : WHITE;
+        if (copyPosition) {
+            // Copy bool property and side
+            enemySide = Position->enemySide;
+            bQCastlingRights = Position->bQCastlingRights;
+            bKCastlingRights = Position->bKCastlingRights;
+            wQCastlingRights = Position->wQCastlingRights;
+            // Copy color and piece arrays with loop
+            for (int i = 0; i < 64;) {
+                color[i] = Position->color[i];
+                piece[i] = Position->piece[i];
+            }
         }
-    };
+    }
 };
+}; // namespace Aurora
