eab68715d315b0c68a4893b980a550928501b081
ShambuKumar <kumarshambu2010@gmail.com>
2023-08-04 10:12:12 +0200

    Changed functions in movegen.h to inline

---
+++ src/movesearch.cpp
@@ -0,0 +1,8 @@
+#include "movegen.h"
+#include <stdio.h>
+namespace Aurora {
+namespace MoveSearch {
+void search(int depth, Position *Position) {}
+
+} // namespace MoveSearch
+} // namespace Aurora
--- src/position.h
+++ src/position.h
@@ -4,30 +4,29 @@
 #include <vector>
 namespace Aurora {
 class Position {
-  public:
-    std::vector<Move> moves;
-    int color[64] = {};
-    int piece[64] = {};
-    int enemySide;
-    bool bQCastlingRights;
-    bool bKCastlingRights;
-    bool wQCastlingRights;
-    bool wKCastlingRights;
-    Position(int side, bool copyPosition = false,
-             Position *Position = nullptr) {
-        enemySide = (side == WHITE) ? BLACK : WHITE;
-        if (copyPosition) {
-            // Copy bool property and side
-            enemySide = Position->enemySide;
-            bQCastlingRights = Position->bQCastlingRights;
-            bKCastlingRights = Position->bKCastlingRights;
-            wQCastlingRights = Position->wQCastlingRights;
-            // Copy color and piece arrays with loop
-            for (int i = 0; i < 64;) {
-                color[i] = Position->color[i];
-                piece[i] = Position->piece[i];
-            }
-        }
+public:
+  std::vector<Move> moves;
+  int color[64] = {};
+  int piece[64] = {};
+  int enemySide;
+  bool bQCastlingRights;
+  bool bKCastlingRights;
+  bool wQCastlingRights;
+  bool wKCastlingRights;
+  Position(int side, bool copyPosition = false, Position *Position = nullptr) {
+    enemySide = (side == WHITE) ? BLACK : WHITE;
+    if (copyPosition) {
+      // Copy bool property and side
+      enemySide = Position->enemySide;
+      bQCastlingRights = Position->bQCastlingRights;
+      bKCastlingRights = Position->bKCastlingRights;
+      wQCastlingRights = Position->wQCastlingRights;
+      // Copy color and piece arrays with loop
+      for (int i = 0; i < 64;) {
+        color[i] = Position->color[i];
+        piece[i] = Position->piece[i];
+      }
     }
+  }
 };
 }; // namespace Aurora
