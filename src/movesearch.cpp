d2ca63d311adcd2d81c7342d5342537917de3ac3
ShambuKumar <kumarshambu2010@gmail.com>
2023-08-06 20:15:22 +0200

    Added experimental recursive search function to movesearch.cpp

--- src/movesearch.cpp
+++ src/movesearch.cpp
@@ -9,6 +9,16 @@ inline Position makeMove(Move Move, Position Position, int side) {
   TempPos.piece[Move.startSquare] = EMPTY;
   return TempPos;
 }
+void search(int depth, int currentDepth, Node Node) {
+  if (currentDepth > depth) {
+    return;
+  }
+  for (int i = 0; i < Node.Children.size();) {
+    Node.Children.push_back(
+        {makeMove(Node.Position.moves[i], Node.Position, WHITE)});
+    search(depth, currentDepth + 1, Node.Children[Node.Children.size() - 1]);
+  }
+}
 
 } // namespace MoveSearch
 } // namespace Aurora
