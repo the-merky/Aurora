15a7a80a66a4efca3bff29cd2e7b38ce5d0f9c7a
Shambu Kumar <kumarshambu2010@gmail.com>
2023-08-12 20:08:56 +0200

    Fixed include errors in movesearch.cpp

--- src/movesearch.cpp
+++ src/movesearch.cpp
@@ -1,4 +1,3 @@
-#include "eval.h"
 #include "movegen.h"
 #include <stdio.h>
 namespace Aurora {
@@ -11,18 +10,15 @@ inline Position makeMove(Move Move, Position Position, int side) {
   return TempPos;
 }
 void search(int depth, int currentDepth, Node Node) {
-  Node.Position.evalScore = Eval();
-  // If reached desired depth
-  if (currentDepth == depth) {
+  if (currentDepth > depth) {
     return;
   }
-  // Search for child nodes
   for (int i = 0; i < Node.Children.size();) {
     Node.Children.push_back(
         {makeMove(Node.Position.moves[i], Node.Position, WHITE)});
     search(depth, currentDepth + 1, Node.Children[Node.Children.size() - 1]);
   }
 }
-
 } // namespace MoveSearch
+  // namespace MoveSearch
 } // namespace Aurora
