b6c42b0b9be9f98d8877b6b2003485aed63362b7
Shambu Kumar <kumarshambu2010@gmail.com>
2023-08-17 16:47:29 +0200

    Remove unnecessary argument from makeMove

--- src/movesearch.cpp
+++ src/movesearch.cpp
@@ -1,10 +1,14 @@
+#include "fen.h"
 #include "movegen.h"
+#include <algorithm>
+#include <iostream>
 #include <stdio.h>
 namespace Aurora {
 namespace MoveSearch {
 
-inline Position makeMove(Move Move, Position Position, int side) {
-  class Position TempPos(side, true, &Position);
+inline Position makeMove(Move Move, Position Position) {
+  class Position TempPos;
+  TempPos.copyPosition(&Position);
   TempPos.piece[Move.targetSquare] = Position.piece[Move.startSquare];
   TempPos.piece[Move.startSquare] = EMPTY;
   return TempPos;
@@ -13,14 +17,25 @@ void search(int depth, int currentDepth, Node Node) {
   if (currentDepth > depth) {
     return;
   }
-  for (int i = 0; i < Node.children.size();) {
-    MoveGen::initializePosition(Node.position);
-    MoveGen::updateAttackedSquares(BLACK);
-    MoveGen::generate(WHITE);
+  std::cout << "currentDepth = " << currentDepth << std::endl;
+  MoveGen::initializePosition(Node.position);
+  MoveGen::updateAttackedSquares(BLACK);
+  MoveGen::generate(WHITE);
+  for (int i = 0; i < Node.position.moves.size();) {
     Node.children.push_back(
-        {makeMove(Node.position.moves[i], Node.position, WHITE)});
+        {.position = makeMove(Node.position.moves[i], Node.position)});
+    std::cout << "A\n";
     search(depth, currentDepth + 1, Node.children[Node.children.size() - 1]);
+    std::cout << "B\n";
   }
 }
 } // namespace MoveSearch
 } // namespace Aurora
+// Test MoveSearch
+int main() {
+  using namespace Aurora;
+  Node TestNode;
+  FEN::parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+             TestNode.position.piece, TestNode.position.color);
+  MoveSearch::search(24, 1, TestNode);
+}
