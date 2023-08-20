88b00d6fe665ed1f36e35a199985f3439089f001
Shambu Kumar <kumarshambu2010@gmail.com>
2023-08-20 16:11:38 +0200

    Remove debug cout statements

--- src/movesearch.cpp
+++ src/movesearch.cpp
@@ -21,18 +21,13 @@ void search(int depth, int currentDepth, Node Node) {
   }
   std::cout << "currentDepth = " << currentDepth << std::endl;
   MoveGen::initializePosition(Node.position);
-  std::cout << "1\n";
   MoveGen::updateAttackedSquares(BLACK);
-  std::cout << "2\n";
   MoveGen::generate(WHITE);
-  std::cout << "3\n";
   for (int i = 0; i < Node.position.moves.size();) {
     Node.children.push_back({});
     makeMove(Node.position.moves[i], Node.position,
              Node.children[Node.children.size() - 1].position);
-    std::cout << "A\n";
     search(depth, currentDepth + 1, Node.children[Node.children.size() - 1]);
-    std::cout << "B\n";
     i++;
   }
 }
@@ -44,5 +39,5 @@ int main() {
   Node TestNode;
   FEN::parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
              TestNode.position.piece, TestNode.position.color);
-  MoveSearch::search(2, 1, TestNode);
+  MoveSearch::search(5, 1, TestNode);
 }
