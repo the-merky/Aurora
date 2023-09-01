da37f2a70d9d5e5e830fe476cb4481fe9cedf2fe
Shambu Kumar <kumarshambu2010@gmail.com>
2023-09-01 18:14:32 +0200

    Moved to test code to new file main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "movesearch.h"
+// Test MoveSearch
+int main() {
+  using namespace Aurora;
+  Node TestNode;
+  FEN::parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+             TestNode.position.piece, TestNode.position.color);
+  MoveSearch::search(5, 1, TestNode);
+}
