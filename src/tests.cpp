cba7e0fc077e352f16cc1dea740870e2353488e7
Shambu Kumar <kumarshambu2010@gmail.com>
2023-09-23 19:00:45 +0200

    Actually remove movesearch test

--- src/tests.cpp
+++ src/tests.cpp
@@ -6,17 +6,5 @@ int main() {
   // Tests
   // Movegen
 
-  // Movesearch
-  std::cout << "Movesearch test:\n";
-  Node TestNode;
-  FEN::parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
-                     TestNode.position.piece, TestNode.position.color); 
-  MoveSearch::search(3, 1, TestNode);
-  if (TestNode.children.size() == 20) {
-    std::cout << "Depth 1: ✔️ Passed\n";
-  } else {
-    std::cout << "Depth 1: ❌Failed - children size(" << TestNode.children.size()
-              << "/20)";
-  } 
   return 0;
 }
